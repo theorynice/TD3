cdd347e3091009e4c7515350ffb38101faa1b309
theorynice <1003759688@qq.com>
2021-01-11 17:02:41 +0800

    20210111

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import torch
-import gym
+from jsbsim_env import jsbsim_env
 import argparse
 import os
 
@@ -8,20 +8,19 @@ import utils
 import TD3
 import OurDDPG
 import DDPG
-
+import pdb
 
 # Runs policy for X episodes and returns average reward
 # A fixed seed is used for the eval environment
 def eval_policy(policy, env_name, seed, eval_episodes=10):
-	eval_env = gym.make(env_name)
-	eval_env.seed(seed + 100)
+	eval_env = jsbsim_env('f16',1./120)
 
 	avg_reward = 0.
 	for _ in range(eval_episodes):
 		state, done = eval_env.reset(), False
 		while not done:
 			action = policy.select_action(np.array(state))
-			state, reward, done, _ = eval_env.step(action)
+			state, reward, done= eval_env.step(action)
 			avg_reward += reward
 
 	avg_reward /= eval_episodes
@@ -29,14 +28,15 @@ def eval_policy(policy, env_name, seed, eval_episodes=10):
 	print("---------------------------------------")
 	print(f"Evaluation over {eval_episodes} episodes: {avg_reward:.3f}")
 	print("---------------------------------------")
+	#pdb.set_trace()
 	return avg_reward
 
 
 if __name__ == "__main__":
 	
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--policy", default="TD3")                  # Policy name (TD3, DDPG or OurDDPG)
-	parser.add_argument("--env", default="HalfCheetah-v2")          # OpenAI gym environment name
+	parser.add_argument("--policy", default="DDPG")                  # Policy name (TD3, DDPG or OurDDPG)
+	parser.add_argument("--env", default="jsbsim_env")          # OpenAI gym environment name
 	parser.add_argument("--seed", default=0, type=int)              # Sets Gym, PyTorch and Numpy seeds
 	parser.add_argument("--start_timesteps", default=25e3, type=int)# Time steps initial random policy is used
 	parser.add_argument("--eval_freq", default=5e3, type=int)       # How often (time steps) we evaluate
@@ -63,16 +63,19 @@ if __name__ == "__main__":
 	if args.save_model and not os.path.exists("./models"):
 		os.makedirs("./models")
 
-	env = gym.make(args.env)
+	env = jsbsim_env('f16',1./120)
 
 	# Set seeds
-	env.seed(args.seed)
+	#env.seed(args.seed)
 	torch.manual_seed(args.seed)
 	np.random.seed(args.seed)
 	
-	state_dim = env.observation_space.shape[0]
-	action_dim = env.action_space.shape[0] 
-	max_action = float(env.action_space.high[0])
+	#state_dim = env.observation_space.shape[0]
+	state_dim=12
+	#action_dim = env.action_space.shape[0] 
+	action_dim=4
+	#max_action = float(env.action_space.high[0])
+	max_action=1
 
 	kwargs = {
 		"state_dim": state_dim,
@@ -102,7 +105,7 @@ if __name__ == "__main__":
 	
 	# Evaluate untrained policy
 	evaluations = [eval_policy(policy, args.env, args.seed)]
-
+	#pdb.set_trace()
 	state, done = env.reset(), False
 	episode_reward = 0
 	episode_timesteps = 0
@@ -114,7 +117,7 @@ if __name__ == "__main__":
 
 		# Select action randomly or according to policy
 		if t < args.start_timesteps:
-			action = env.action_space.sample()
+			action = env.action_sample()
 		else:
 			action = (
 				policy.select_action(np.array(state))
@@ -122,8 +125,8 @@ if __name__ == "__main__":
 			).clip(-max_action, max_action)
 
 		# Perform action
-		next_state, reward, done, _ = env.step(action) 
-		done_bool = float(done) if episode_timesteps < env._max_episode_steps else 0
+		next_state, reward, done = env.step(action) 
+		done_bool = float(done) if episode_timesteps < 1e10 else 0
 
 		# Store data in replay buffer
 		replay_buffer.add(state, action, next_state, reward, done_bool)
